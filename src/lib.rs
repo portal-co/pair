4df4477e7b419c147adc3f73ee89e9af340dbcdc
Graham Kelly <gkgoat6700@gmail.com>
2023-12-26 14:12:15 -0500

    named fields

--- src/lib.rs
+++ src/lib.rs
@@ -85,7 +85,7 @@ impl<T: 'static, Y: 'static, R: 'static, D: 'static> IntoIterator for Params<T, 
 
 pub enum ValueDef<T, Y, R, D> {
     Param(usize),
-    Emit(T, Params<T, Y, R, D>, Y, BTreeSet<Id<ValueDef<T,Y,R,D>>>),
+    Emit{op:T, params: Params<T, Y, R, D>, typ: Y, after: BTreeSet<Id<ValueDef<T,Y,R,D>>>},
     Alias(Value<T, Y, R, D>),
 }
 
@@ -93,8 +93,11 @@ impl<T: Clone, Y: Clone, R: Clone, D> Clone for ValueDef<T, Y, R, D> {
     fn clone(&self) -> Self {
         match self {
             Self::Param(arg0) => Self::Param(arg0.clone()),
-            Self::Emit(arg0, arg1, arg2,arg3) => Self::Emit(arg0.clone(), arg1.clone(), arg2.clone(),arg3.clone()),
+            // Self::Emit(arg0, arg1, arg2,arg3) => Self::Emit(arg0.clone(), arg1.clone(), arg2.clone(),arg3.clone()),
             Self::Alias(arg0) => Self::Alias(arg0.clone()),
+            // ValueDef::Param(_) => todo!(),
+            ValueDef::Emit { op, params, typ, after } => ValueDef::Emit { op: op.clone(), params: params.clone(), typ: typ.clone(), after: after.clone() },
+            // ValueDef::Alias(_) => todo!(),
         }
     }
 }
