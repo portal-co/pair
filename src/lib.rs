fd92683b02fb4a94cf9d7f7b808bd6accdccf4f2
Graham Kelly <gkgoat6700@gmail.com>
2023-12-26 10:12:29 -0500

    rust fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
 
 use id_arena::{Arena, Id};
 
-
 pub struct Value<T, Y, R, D> {
     pub id: Id<ValueDef<T, Y, R, D>>,
     pub idx: Option<u32>,
@@ -144,14 +143,14 @@ pub struct ModuleTransform<T, Y, R, D, S, L, E, P> {
     pub code_cache: BTreeMap<Id<Fun<T, Y, R, D>>, Id<Fun<S, L, E, P>>>,
 }
 pub struct FuncTransformCtx<T, Y, R, D, S, L, E, P> {
-    pub input: Id<Fun<T,Y,R,D>>,
-    pub output: Id<Fun<S,L,E,P>>,
+    pub input: Id<Fun<T, Y, R, D>>,
+    pub output: Id<Fun<S, L, E, P>>,
 }
 pub trait MTBehavior<T, Y, R, D, S, L, E, P> {
     fn value(
         &mut self,
         ctx: &mut ModuleTransform<T, Y, R, D, S, L, E, P>,
-        fun_ctx: FuncTransformCtx<T,Y,R,D,S,L,E,P>,
+        fun_ctx: FuncTransformCtx<T, Y, R, D, S, L, E, P>,
         def: ValueDef<T, Y, R, D>,
         value: impl FnMut(
             &mut Self,
@@ -168,7 +167,7 @@ pub trait MTBehavior<T, Y, R, D, S, L, E, P> {
     fn terminator(
         &mut self,
         ctx: &mut ModuleTransform<T, Y, R, D, S, L, E, P>,
-        fun_ctx: FuncTransformCtx<T,Y,R,D,S,L,E,P>,
+        fun_ctx: FuncTransformCtx<T, Y, R, D, S, L, E, P>,
         def: R,
         value: impl FnMut(
             &mut Self,
@@ -196,7 +195,9 @@ pub trait MTBehavior<T, Y, R, D, S, L, E, P> {
 }
 pub type ValueTransMap<T, Y, R, D, S, L, E, P> =
     Rc<RefCell<BTreeMap<Id<ValueDef<T, Y, R, D>>, Id<ValueDef<S, L, E, P>>>>>;
-impl<T: Clone, Y: Clone, R: Clone, D: Clone, S, L, E: Default, P> ModuleTransform<T, Y, R, D, S, L, E, P> {
+impl<T: Clone, Y: Clone, R: Clone, D: Clone, S, L, E: Default, P>
+    ModuleTransform<T, Y, R, D, S, L, E, P>
+{
     pub fn func_value<W: MTBehavior<T, Y, R, D, S, L, E, P>>(
         &mut self,
         w: &mut W,
@@ -218,11 +219,14 @@ impl<T: Clone, Y: Clone, R: Clone, D: Clone, S, L, E: Default, P> ModuleTransfor
             };
             w.value(
                 self,
-                FuncTransformCtx { input: f.clone(), output: *self.code_cache.get(&f).unwrap() },
+                FuncTransformCtx {
+                    input: f.clone(),
+                    output: *self.code_cache.get(&f).unwrap(),
+                },
                 self.input.code[f].values[a].clone(),
                 |w, t, v| t.func_value(w, f, m.clone(), v),
                 |w, t, f| t.func(w, f),
-                |w,t,d|t.dat(w, d),
+                |w, t, d| t.dat(w, d),
             )
         };
         self.out.code[*self.code_cache.get(&f).unwrap()].values[i] = v;
@@ -243,21 +247,29 @@ impl<T: Clone, Y: Clone, R: Clone, D: Clone, S, L, E: Default, P> ModuleTransfor
         let a = &self.input.code[f.clone()];
         let t = w.terminator(
             self,
-            FuncTransformCtx { input: f.clone(), output: me.clone() },
+            FuncTransformCtx {
+                input: f.clone(),
+                output: me.clone(),
+            },
             self.input.code[f].terminator.clone(),
             |w, t, v| t.func_value(w, f, m.clone(), v),
             |w, t, f| t.func(w, f),
-            |w,t,d|t.dat(w, d),
+            |w, t, d| t.dat(w, d),
         );
         self.out.code[me].terminator = t;
         return me;
         // });
     }
-    pub fn dat<W: MTBehavior<T, Y, R, D, S, L, E, P>>(&mut self, w: &mut W, dd: Id<D>) -> Id<P>{
-        if let Some(b) = self.datum_cache.get(&dd){
+    pub fn dat<W: MTBehavior<T, Y, R, D, S, L, E, P>>(&mut self, w: &mut W, dd: Id<D>) -> Id<P> {
+        if let Some(b) = self.datum_cache.get(&dd) {
             return *b;
         }
-        let d = w.datum(self, self.input.data[dd].clone(), |w,t,f|t.func(w,f), |w,t,d|t.dat(w, d));
+        let d = w.datum(
+            self,
+            self.input.data[dd].clone(),
+            |w, t, f| t.func(w, f),
+            |w, t, d| t.dat(w, d),
+        );
         let e = self.out.data.alloc(d);
         self.datum_cache.insert(dd, e);
         return e;
